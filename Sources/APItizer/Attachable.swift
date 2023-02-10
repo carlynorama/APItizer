ba406f45fcbb24abd5fc09c9d476e90400d9da90
carlynorama <carlynorama@gmail.com>
2023-02-10 15:53:44 -0800

    white space

--- Sources/APItizer/Attachable.swift
+++ Sources/APItizer/Attachable.swift
@@ -11,8 +11,8 @@ public protocol Attachable  {
     var attachmentName:String { get }
     var mimeType:String { get }
     var data:Data { get }
-    
 }
+
 extension Attachable {
     var base64EncondedString: String {
         return "data:\(mimeType);base64,\(data.base64EncodedString())"
