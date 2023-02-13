534ae70a9a1d266d2db6a6633b7ac3932c822221
carlynorama <carlynorama@gmail.com>
2023-02-13 08:37:29 -0800

    Move error into own file

---
+++ Sources/APItizer/Errors.swift
@@ -0,0 +1,22 @@
+//
+//  Errors.swift
+//  
+//
+//  Created by Carlyn Maw on 10/30/22 as part of ActivityPubExplorer
+//
+
+
+import Foundation
+
+
+enum APItizerError: Error, CustomStringConvertible {
+    case message(String)
+    public var description: String {
+        switch self {
+        case let .message(message): return message
+        }
+    }
+    init(_ message: String) {
+        self = .message(message)
+    }
+}
