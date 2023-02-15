a0c7e795bb4a2ac456f8554826c2156a811a15f2
carlynorama <carlynorama@gmail.com>
2023-02-14 16:34:51 -0800

    comment out print

--- Sources/APItizer/APIService+Auth.swift
+++ Sources/APItizer/APIService+Auth.swift
@@ -8,9 +8,10 @@
 import Foundation
 
 public extension APIService where Self:Authorizable {
-    public func addAuth(request: inout URLRequest) throws {
-        print(self)
-        print("addAuth: hasValidToken \(self.hasValidToken)")
+    func addAuth(request: inout URLRequest) throws {
+        //print(self)
+        //might also consider checking validation. 
+        //print("addAuth: hasValidToken \(self.hasValidToken)")
         if let auth = self.authentication {
             try auth.addBearerToken(to:&request)
         } else {
