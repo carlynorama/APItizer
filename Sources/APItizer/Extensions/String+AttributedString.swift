c6b14d50b3a0faf23c102df860c69d7aa30b4870
carlynorama <carlynorama@gmail.com>
2023-05-12 16:59:02 -0700

    temp comment out AttributedString stuff

--- Sources/APItizer/Extensions/String+AttributedString.swift
+++ Sources/APItizer/Extensions/String+AttributedString.swift
@@ -1,60 +1,67 @@
-//
-//  APItizer
-//  https://github.com/carlynorama/APItizer
-//
-//  String+AttributedString.swift
-//  Created by Carlyn Maw on 10/30/22.
-//
-//
-import Foundation
+// //
+// //  APItizer
+// //  https://github.com/carlynorama/APItizer
+// //
+// //  String+AttributedString.swift
+// //  Created by Carlyn Maw on 10/30/22.
+// //
+// //
+// if !os(Linux)
+// import Foundation
 
-// print(String(data: data, encoding: .utf8)!)
+// // print(String(data: data, encoding: .utf8)!)
 
-public extension String {
-    //Usage
-    //List(ExampleText.harderHTMLTests, id:\.self) { item in
-    //    Text(item.listCrasher() ?? "Nothing to see").id(UUID()) <- Will need id() somewhere if view scrolls
-    //}
-    //Parses emoji's correctly by adding characterEncoding option.
-    func parseAsHTML() -> AttributedString? {
-        let data = Data(self.utf8)
-        return try? AttributedString(NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html,  .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil))
-     }
+// public extension String {
     
-    //Usage
-    //List(ExampleText.harderHTMLTests, id:\.self) { item in
-    //    Text(item.htmlToAttributedString()).id(UUID())  <-Will need id() somewhere if view scrolls
-    //}
-    func catchingParseAsHTML() -> AttributedString {
-        do {
-            let data = Data(self.utf8)
-            let result = try AttributedString(NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html,  .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil))
-            return result
-        } catch {
-            return AttributedString("Error parsing HTML: \(error)")
-        }
-    }
+//     //Usage
+//     //List(ExampleText.harderHTMLTests, id:\.self) { item in
+//     //    Text(item.listCrasher() ?? "Nothing to see").id(UUID()) <- Will need id() somewhere if view scrolls
+//     //}
+//     //Parses emoji's correctly by adding characterEncoding option.
+//     func parseAsHTML() -> AttributedString? {
+//         let data = Data(self.utf8)
+//         return try? AttributedString(NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html,  .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil))
+//      }
     
-    func parseAsMarkdown() -> AttributedString? {
-        return try? AttributedString(markdown: self)
-    }
     
-    func catchingParseAsMarkdown() -> AttributedString {
-        do {
-            return try AttributedString(markdown: self)
-        } catch {
-            return AttributedString("Error parsing HTML: \(error)")
-        }
-    }
+//     //Usage
+//     //List(ExampleText.harderHTMLTests, id:\.self) { item in
+//     //    Text(item.htmlToAttributedString()).id(UUID())  <-Will need id() somewhere if view scrolls
+//     //}
+//     func catchingParseAsHTML() -> AttributedString {
+//         do {
+//             let data = Data(self.utf8)
+//             let result = try AttributedString(NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html,  .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil))
+//             return result
+//         } catch {
+//             return AttributedString("Error parsing HTML: \(error)")
+//         }
+//     }
     
-    func linkedText(url: URL) -> AttributedString {
-        var attributes = AttributeContainer()
-        attributes.link = url
+    
+    
+//     func parseAsMarkdown() -> AttributedString? {
+//         return try? AttributedString(markdown: self)
+//     }
+    
+//     func catchingParseAsMarkdown() -> AttributedString {
+//         do {
+//             return try AttributedString(markdown: self)
+//         } catch {
+//             return AttributedString("Error parsing HTML: \(error)")
+//         }
+//     }
+    
+    
+//     func linkedText(url: URL) -> AttributedString {
+//         var attributes = AttributeContainer()
+//         attributes.link = url
 
-        let link = AttributedString(self, attributes: attributes)
+//         let link = AttributedString(self, attributes: attributes)
 
-        var string = AttributedString()
-        string.append(link)
-        return string
-    }
-}
+//         var string = AttributedString()
+//         string.append(link)
+//         return string
+//     }
+// }
+// #endif
